3ad8f2b97f4fb92438e771da9a533940b4822266
Karthiknaya <108059989+Karthiknaya@users.noreply.github.com>
2023-12-28 20:31:13 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -7,7 +7,7 @@ const DBConnection = async () => {
     const USERNAME = process.env.DB_USERNAME;
     const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URL =`mongodb://karthiknayak:karthikfordatabaseeasyshareapp@ac-7dxcixq-shard-00-00.svzgtlj.mongodb.net:27017,ac-7dxcixq-shard-00-01.svzgtlj.mongodb.net:27017,ac-7dxcixq-shard-00-02.svzgtlj.mongodb.net:27017/?ssl=true&replicaSet=atlas-h93e9w-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const MONGO_URL =`mongodb://user:pass@ac-7dxcixq-shard-00-00.svzgtlj.mongodb.net:27017,ac-7dxcixq-shard-00-01.svzgtlj.mongodb.net:27017,ac-7dxcixq-shard-00-02.svzgtlj.mongodb.net:27017/?ssl=true&replicaSet=atlas-h93e9w-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(MONGO_URL, { useNewUrlParser: true });
         console.log('Database connected successfully');
